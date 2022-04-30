2d3c395b5ca3e1f2cae1164a0a1c610661d904a6
Mr. Joe <radics.pepe@gmail.com>
2022-04-30 16:26:39 +0200

    first save 04.30

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Menu code
+
+
+
+
+/* const menuButton = document.getElementById("js-menu")
+const menuButtonX = document.getElementById ("js-menuX")
+
+menuButton.addEventListener("click", menuOpen);
+
+
+function menuOpen() {
+    document.getElementById("js-menuList").style.display ="flex";
+    menuButton.style.display ="none"
+    menuButtonX.style.display =""
+
+} */
